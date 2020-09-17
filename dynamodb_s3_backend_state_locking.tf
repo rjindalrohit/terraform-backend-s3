50055ac98625ec465f046f2452ba1b173e11f768
Rohit Kumar <r.jindalrohit@gmail.com>
2020-09-17 13:17:39 +0100

    Updated lifecycle for dynamodb

--- dynamodb_s3_backend_state_locking.tf
+++ dynamodb_s3_backend_state_locking.tf
@@ -4,10 +4,12 @@ resource "aws_dynamodb_table" "terraform_backend_state_locking"{
   write_capacity = 5
   hash_key       = "LockID"
 
-
   attribute {
     name = "LockID"
     type = "S"
+  }
+  lifecycle {
+    prevent_destroy = true
   }
   tags = {
     Name = "terraform_backend_state_locking"
