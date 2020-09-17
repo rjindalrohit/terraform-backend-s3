15813cb90e3a2c217b6d8707908d773ab9b7cb99
Rohit Kumar <r.jindalrohit@gmail.com>
2020-09-17 15:04:08 +0100

    corrected the backend output syntax

--- output.tf
+++ output.tf
@@ -2,10 +2,10 @@ output "s3_backend_config_block" {
   value =  <<EOT
 
 backend "s3" {
-  bucket = ${aws_s3_bucket.s3_bucket_for_backend.id}
+  bucket = "${aws_s3_bucket.s3_bucket_for_backend.id}"
   key    = "terraform.tfstate"
-  region = ${var.region}
-  dynamodb_table = ${aws_dynamodb_table.terraform_backend_state_locking.id}
+  region = "${var.region}"
+  dynamodb_table = "${aws_dynamodb_table.terraform_backend_state_locking.id}"
   encrypt = true
 }
 EOT
